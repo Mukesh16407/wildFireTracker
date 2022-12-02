90b51d6464618569a81bcbf56b7f5caa19804487
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-12-02 23:26:02 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+export const Footer = () => {
+  return (
+    <div className="header">
+      <p>Created by Ishan</p>
+    </div>
+  );
+};
---
+++ src/map.jsx
@@ -0,0 +1,10 @@
+import googleMapReact from "google-map-react";
+import { React } from "react";
+
+export const Map = ({ center, zoom }) => {
+  return (
+    <div className="map">
+      <googleMapReact>bootstrapUrlKeys=</googleMapReact>
+    </div>
+  );
+};
